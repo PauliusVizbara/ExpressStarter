2ac9a8d42fba36b05d5955c4f1956b8827ccb46f
Paulius Vizbara <paulius.vizbara@telesoftas.com>
2020-06-24 20:08:33 +0300

    chore: routes code cleanup

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,21 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-import bookRoute from './book'
-
-
 export default  (app) => {
-    // app.use('book', bookRoute)
-
-    // API routes
-    // console.log("/////////////////////////////////////////////////////////////////////////////////////////////////////")
     fs.readdirSync(__dirname).forEach(async (file) => {
         if ( file !== 'index.ts') {
             const route = await import(path.join(__dirname, file));
             app.use('/book', route.default)
 
         }
-        // route.default()
-        // app.use(file, route.default())
+
     });
 };
