0306b2e289e27a43d6efee87b6c98ec7f9be5d5d
Paulius Vizbara <paulius.vizbara@telesoftas.com>
2020-06-27 12:41:58 +0300

    fix: routing naming in route generator

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -5,7 +5,7 @@ export default  (app) => {
     fs.readdirSync(__dirname).forEach(async (file) => {
         if ( file !== 'index.ts') {
             const route = await import(path.join(__dirname, file));
-            app.use('/book', route.default)
+            app.use(`/${file.split('.')[0]}`, route.default)
         }
     });
 };
