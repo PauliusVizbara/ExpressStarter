281e9cbaef1a803b191af901ae28bf9ba625ae32
Paulius Vizbara <paulius.vizbara@telesoftas.com>
2020-06-30 18:36:33 +0300

    chore: missing eslint dependencies

--- src/api/websockets/wssController.ts
+++ src/api/websockets/wssController.ts
@@ -1,16 +1,15 @@
 import {Container} from "typedi";
+import WebSocket from "ws";
 
 export class WSSController{
     constructor() {
         const {instance : wss, connections} : any  = Container.get('wss')
-        wss.on('connection', (ws: any) => {
-
-            connections.push(ws)
+        wss.on('connection', (ws: WebSocket) => {
             ws.on('message', (message) => this.onMessage(ws,message));
         })
     }
 
     onMessage = (ws, message) => {
-        ws.send(`You just sent me a ${message}`)
+        ws.send(`Server received: ${message}`)
     }
 }
